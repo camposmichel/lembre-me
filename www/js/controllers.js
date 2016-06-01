5d216fd2df02953875d2d7828ec270dbceeb817e
MCamposYD <michelcsilva@live.com>
2016-06-01 15:14:41 -0300

    Modal e popus para interações com usuário.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('TasksController', function($scope, Tasks, $ionicListDelegate, $cordovaCalendar, $location) {
+.controller('TasksController', function($scope, Tasks, $ionicListDelegate, $cordovaCalendar, $location, $ionicModal, $ionicPopup) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -8,6 +8,13 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+  $ionicModal.fromTemplateUrl('templates/modals/new-task.modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modalNewtask = modal;
+  });
 
   $scope.tasks = Tasks.all();
   $scope.remove = function(chat) {
@@ -27,12 +34,34 @@ angular.module('starter.controllers', [])
     });
   };
 
-  $scope.testemichel = function(){
-    alert('OK');
-  };
-
   $scope.goToTask = function(taskId){
     $location.path('/app/tasks/' + taskId);
+  };
+
+  $scope.changePriority = function(task){
+    $ionicPopup.show({
+      title: 'Alterar prioridade',
+      scope: $scope,
+      buttons: [{
+        text: '<i class="icon ion-arrow-graph-up-right"></i>',
+        type: 'button-energized',
+        onTap: function(e) {
+          console.log(JSON.stringify(e));
+        }
+      },{
+        text: '<i class="icon ion-arrow-graph-up-right"></i>',
+        type: 'button-stable buttonPriorityMedium',
+        onTap: function(e) {
+          console.log(JSON.stringify(e));
+        }
+      },{
+        text: '<i class="icon ion-arrow-graph-up-right"></i>',
+        type: 'button-assertive',
+        onTap: function(e) {
+          console.log(JSON.stringify(e));
+        }
+      }]
+    });
   };
 })
 
@@ -40,7 +69,7 @@ angular.module('starter.controllers', [])
   $scope.task = Tasks.get($stateParams.chatId);
 })
 
-.controller('AdminTasksController', function($scope, Tasks, $location) {
+.controller('AdminTasksController', function($scope, Tasks, $location, $ionicPopup) {
 
   $scope.setDateToToday = function(){
     $scope.newTask = { startDate: new Date(), endDate: new Date() };
@@ -55,5 +84,27 @@ angular.module('starter.controllers', [])
     $scope.setDateToToday();
 
     $location.path('/app/tasks');
+  };
+
+  $scope.testemichel = function(){
+    alert('FUNFANDO');
+  };
+
+  $scope.createCategory = function() {
+    $ionicPopup.show({
+      template: '<input type="text" ng-model="teste">',
+      title: 'Nome da nova categoria',
+      subTitle: 'Tente usar apenas uma palavra',
+      scope: $scope,
+      buttons: [{
+        text: '<i class="icon ion-close"></i>'
+      }, {
+        text: '<i class="icon ion-checkmark"></i>',
+        type: 'button-balanced',
+        onTap: function(e) {
+          console.log(JSON.stringify(e));
+        }
+      }]
+    });
   };
 });
